4edf95eff8031c87a51ac560630c79916a9215b9
chasmit <chasmith813@gmail.com>
2025-09-24 16:47:36 -0400

    requests begin rework of For Patients page - add tab menu and new component #92896

---
+++ src/components/custom/custom-new-braces/custom-new-braces.vue
@@ -0,0 +1,26 @@
+<template lang="pug" src="./custom-new-braces.pug"></template>
+
+<script>
+export default {
+  props: {
+    props: {
+      type: Array,
+      default: () => []
+    }
+  },
+
+  methods: {
+    getYouTubeId (url) {
+      if (!url) return null
+      const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/
+      const match = url.match(regExp)
+
+      return (match && match[2].length === 11) ? match[2] : null
+    },
+    getYouTubeEmbedUrl (url) {
+      const videoId = this.getYouTubeId(url)
+      return videoId ? `https://www.youtube.com/embed/${videoId}?rel=0&modestbranding=1` : ''
+    }
+  }
+}
+</script>
--- src/pages/patients/patients.vue
+++ src/pages/patients/patients.vue
@@ -1,34 +1,107 @@
 <template lang="pug" src="./patients.pug"></template>
 
 <script>
-import HeroImg from '@/components/block/block-hero-photo/block-hero-photo'
 import Forms from '@/components/custom/custom-patients-forms/custom-patients-forms'
-import Rewards from '@/components/custom/custom-patients-rewards/custom-patients-rewards'
-import Payment from '@/components/custom/custom-patients-payment/custom-patients-payment'
+import NewBraces from '@/components/custom/custom-new-braces/custom-new-braces'
 
 export default {
+  data () {
+    return {
+      activeIndex: 0,
+      tabs: [
+        { title: 'New Braces Info', key: 'new-braces' },
+        { title: 'Patient Forms', key: 'forms' },
+        { title: 'Early Smiles Club', key: 'early-smiles' }
+        /*
+        { title: 'Temporary Relief Tips', key: 'temporary-relief' },
+        { title: 'Events', key: 'events' }
+         */
+      ]
+    }
+  },
   computed: {
     props () {
       return this.$store.state.pages['for-patients']
+    },
+    activeTab () {
+      return this.tabs[this.activeIndex]
+    }
+  },
+  methods: {
+    updateActiveTab (index) {
+      this.activeIndex = index
     }
   },
   components: {
-    HeroImg,
     Forms,
-    Rewards,
-    Payment
+    NewBraces
   }
 }
 </script>
-<style>
-    .info-flyer {
-      margin: 0 auto;
-      max-width: 100%;
+
+<style lang="scss">
+$green: #94c733;
+.nav-spacer {
+  height: 120px;
+}
+
+.pages-patients {
+  &__col-full {
+    display: flex;
+    justify-content: center;
+    margin-top: 1rem;
+  }
+
+  &__title-container {
+    margin: 0 1rem;
+    cursor: pointer;
+    color: #696969;
+
+    &:first-of-type {
+      margin-left: 0;
     }
-    .info-flyer__container {
-      display: inline-block;
-      align-items: center;
-      text-align: center;
-      margin-bottom: 4rem;
+
+    &:last-of-type {
+      margin-right: 0;
     }
+
+    &--active {
+      color: $green;
+
+      &:hover {
+        color: $green !important;
+      }
+
+      .pages-patients__icon {
+        display: block;
+      }
+    }
+
+    &:hover {
+      color: black;
+    }
+  }
+
+  &__icon {
+    margin-top: -10px;
+    display: none;
+    fill: $green;
+  }
+
+  &__content {
+    margin-top: 2rem;
+  }
+}
+
+.info-flyer {
+  margin: 0 auto;
+  max-width: 100%;
+}
+
+.info-flyer__container {
+  display: inline-block;
+  align-items: center;
+  text-align: center;
+  margin-bottom: 4rem;
+}
 </style>
